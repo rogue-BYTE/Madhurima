4b3ff902ec2a6f2dff192d2c5b560e96d8e2ecc1
Vedant Anand <33545737+rogue-BYTE@users.noreply.github.com>
2022-10-07 21:23:15 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import './footer.css'
-import { Link, useLocation } from 'react-router-dom'
 
 function Footer() {
-  const activeRoute = useLocation().pathname;
   return (
     <div className='footer'>
       Copyright Madhurima Ekka | All Rights Reserved | Designed by Vedant Anand
