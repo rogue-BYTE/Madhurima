6f648776225d40635c4f3f3aa21a0dd5a9e2f0b3
Vedant Anand <33545737+rogue-BYTE@users.noreply.github.com>
2022-10-07 21:05:50 +0530

    Update exploreHeader.js

--- src/components/exploreHeader.js
+++ src/components/exploreHeader.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import './exploreHeader.css'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 function ExploreHeader() {
+  const activeRoute = useLocation().pathname;
   return (
     <div className='ExploreHeader'>
       <div className='nav-item-container'>
@@ -17,14 +18,14 @@ function ExploreHeader() {
       </div>
       <div className='nav-item-container'>
         <Link
-          className='nav-item'
+          className={"nav-item " + (activeRoute === '/explore' ? 'nav-item-active' : '')}
           to='/explore'
           style={{ textDecoration: 'none', color: 'black' }}
         >
           Explore
         </Link>
         <Link
-          className='nav-item'
+          className={"nav-item " + (activeRoute === '/about' ? 'nav-item-active' : '')}
           to='/about'
           style={{ textDecoration: 'none', color: 'black' }}
         >
